264cc5c9f69ed7c783c30074891bf90ecd28c1d9
yoshimaru <yoshi0410777@gmail>
2025-09-01 16:32:43 +0900

    add(records):記録一覧のルーティング・コントローラ・ビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get "records/index"
+  get "records/new"
+  get "records/show"
   get "up" => "rails/health#show", as: :rails_health_check
   # 公開トップページ
   root "top#index"
 
   resources :groups,  only: [:index]
+  resources :records, only: [:index, :new, :create, :show]
 end
