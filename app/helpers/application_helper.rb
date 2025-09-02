1c5902dff1788030324192e7c2d124677fb6bed2
yoshimaru <yoshi0410777@gmail>
2025-09-02 11:54:50 +0900

    fix: コンフリクト解消（application_helper.rb / application.html.erb / routes.rb）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,15 @@
 module ApplicationHelper
-  # /app/assets/images/icons/<name>.svg をインライン描画
   def inline_svg(name, classes: "w-6 h-6", aria_hidden: true)
     path = Rails.root.join("app/assets/images/icons/#{name}.svg")
     return "(missing #{name}.svg)" unless File.exist?(path)
 
     svg = File.read(path)
-    # class 付与（既存があっても上書きでOKにする簡易版）
+
     svg.sub!(/<svg\b/, "<svg class=\"#{classes}\"")
     svg.sub!(/<svg\b/, "<svg aria-hidden=\"true\" focusable=\"false\"") if aria_hidden
     svg.html_safe
   end
 
-  # フッターナビのリンク（アイコン＋ラベル／現在地で色強調）
   def footer_nav_link_to(path, label, icon:, active_when: nil, **opts)
     active = active_when.nil? ? current_page?(path) : !!active_when
     base   = "flex flex-col items-center justify-center gap-1 py-3 text-xs select-none"
@@ -21,4 +19,5 @@ module ApplicationHelper
       concat content_tag(:span, label, class: "leading-none")
     end
   end
+end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get "mypages/show"
-  get "mypages/edit"
-  get "records/index"
-  get "records/new"
-  get "records/show"
   get "up" => "rails/health#show", as: :rails_health_check
   # 公開トップページ
   root "top#index"
