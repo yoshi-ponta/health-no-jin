5b7444e7a8e3882852aca5d468ae0767acf234e8
yoshimaru <yoshi0410777@gmail>
2025-09-07 16:08:37 +0900

    chore(routes): ルート調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-  # 公開トップページ
+  if defined?(Devise)
+  devise_for :users
+
+    # 未ログイン → / (Top)
+    unauthenticated :user do
+      root to: "top#index", as: :unauthenticated_root
+    end
+
+    # ログイン済み → / (Home)
+    authenticated :user do
+      root to: "home#index", as: :authenticated_root
+    end
+  end
+
   root "top#index"
+
+  # 開発用（直接アクセスしたい時用）
+  get :top,  to: "top#index"
+  get :home, to: "home#index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
 
   resources :groups,  only: [ :index ]
   resources :records, only: [ :index, :new, :create, :show ]
