8fbde037c23e0c8bcf6ae4b6becb4f8eabc0f428
yoshimaru <yoshi0410777@gmail>
2025-09-17 16:24:49 +0900

    feat: Userモデルににグループ関連を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  has_many :owned_groups, class_name: "Group", foreign_key: "owner_id", dependent: :nullify
+  has_many :group_memberships, dependent: :destroy
+  has_many :groups, through: :group_memberships
 end
