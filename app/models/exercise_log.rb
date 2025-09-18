5e0819a992708e191efeb2f303ab6ddb7690f1f4
yoshimaru <yoshi0410777@gmail>
2025-09-19 07:31:59 +0900

    feat:モデル整備（ExerciseLogの関連とバリデーション追加、Userのhas_many追加、ExerciseItem追加）

--- app/models/exercise_log.rb
+++ app/models/exercise_log.rb
@@ -1,4 +1,7 @@
 class ExerciseLog < ApplicationRecord
   belongs_to :user
-  belongs_to :group
+  belongs_to :exercise_item
+
+  validates :performed_at, presence: true
+  validates :amount, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
 
   has_many :owned_groups, class_name: "Group", foreign_key: "owner_id", dependent: :nullify
   has_many :group_memberships, dependent: :destroy
+  has_many :exercise_logs, dependent: :destroy
   has_many :groups, through: :group_memberships
 end
