ef30afe039f88e664835f7aa41fd05cbefe25f72
yoshimaru <yoshi0410777@gmail>
2025-09-27 23:33:36 +0900

    fix:RuboCopの自動修正

--- db/migrate/20250918063927_align_exercise_item_id_on_logs.rb
+++ db/migrate/20250918063927_align_exercise_item_id_on_logs.rb
@@ -2,7 +2,7 @@ class AlignExerciseItemIdOnLogs < ActiveRecord::Migration[8.0]
   def change
     change_column :exercise_logs, :exercise_item_id, :bigint
 
-    add_foreign_key :exercise_logs, :exercise_items,column: :exercise_item_id, validate: false unless
+    add_foreign_key :exercise_logs, :exercise_items, column: :exercise_item_id, validate: false unless
       foreign_key_exists?(:exercise_logs, :exercise_items, column: :exercise_item_id)
 
     add_index :exercise_logs, :exercise_item_id unless
