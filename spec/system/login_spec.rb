d762109d1a0f3a8c9e5f0d45e22f6b7f90cb22f5
yoshimaru <yoshi0410777@gmail>
2025-10-07 00:23:32 +0900

    test: ログイン導線のテストを追加

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'ログイン導線', type: :system do
   describe 'ログインしてホームに到達' do
-    it '到達できる' do end
+    it '到達できる' do
+      user = create(:user)
+
+      visit new_user_session_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'ログイン'
+    end
   end
 end
