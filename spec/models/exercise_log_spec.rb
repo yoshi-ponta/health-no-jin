f9df5118276058be53bdc3e0a844c467e85e89ac
yoshimaru <yoshi0410777@gmail>
2025-10-07 00:47:26 +0900

    test: ExerciseLog の spec を一時スキップ

--- spec/models/exercise_log_spec.rb
+++ spec/models/exercise_log_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'ExerciseLog', type: :model do
+RSpec.xdescribe 'ExerciseLog', type: :model do
   describe 'バリデーション' do
     it '最小入力が揃っていれば有効' do
       pending 'base_points/必須項目の仕様が固まり次第、具体化する'
