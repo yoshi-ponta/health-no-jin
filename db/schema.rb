b5420fc8f0223d9185b73cb66e943e97e0c2a11f
yoshimaru <yoshi0410777@gmail>
2025-10-09 10:43:21 +0900

    add:messages テーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_08_041633) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_08_132707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -84,6 +84,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_08_041633) do
     t.string "invite_token"
     t.index ["invite_token"], name: "index_groups_on_invite_token", unique: true
     t.index ["owner_id"], name: "index_groups_on_owner_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
