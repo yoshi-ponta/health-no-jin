e74d65bd6673cf4d91e4c223ff76b107b3edf319
yoshimaru <yoshi0410777@gmail>
2025-10-14 21:43:11 +0900

    feat: グループ画面を index に集約し、在籍メンバーの一覧を表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,4 +9,15 @@ class Group < ApplicationRecord
   def ensure_invite_token
     self.invite_token ||= SecureRandom.urlsafe_base64(24)
   end
+
+  def total_points(range: nil)
+    logs = ExerciseLog
+      .joins(user: :group_memberships)
+      .where(group_memberships: { group_id: id })
+      .where("exercise_logs.performed_at >= group_memberships.joined_at")
+      .where("group_memberships.left_at IS NULL OR exercise_logs.performed_at < group_memberships.left_at")
+
+    logs = logs.where(performed_at: range) if range
+    logs.sum(:points)
+  end
 end
