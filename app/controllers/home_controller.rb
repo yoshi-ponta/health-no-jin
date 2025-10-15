1770e0391bf0c47817c0b7468a2ffbb78fd9d8bf
yoshimaru <yoshi0410777@gmail>
2025-10-15 23:14:33 +0900

    feat:HomeControllerでグループ合計ポイントをGroup#total_pointsに代入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @group_total_points =
+      if @active_group
+        @active_group.total_points
+      else
+        0
+      end
   end
 end
