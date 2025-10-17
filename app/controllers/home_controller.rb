b7e94176bd51a330e70252a7e8216c908276c553
yoshimaru <yoshi0410777@gmail>
2025-10-17 23:21:05 +0900

    refactor: 進捗バーの計算を Group#progress_percent にし配線を簡素化

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,9 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if @active_group
-      @progress = Progress::Builder.call(@active_group)
-    else
-      @progress = Progress::Dto.new(total_points: 0, progress_percent: 0, stage_limits: CastleHelper::CASTLE_LEVELS)
+    @active_group = current_user.groups.first
+    if @active_group.present?
+      @progress = @active_group
     end
   end
 end
