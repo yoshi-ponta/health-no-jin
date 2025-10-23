f743fc5a63c114c8dbd5a05d6bd564aa9a022500
yoshimaru <yoshi0410777@gmail>
2025-10-23 11:53:17 +0900

    test:ユーザーの名前を必須にするテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,26 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe 'バリデーション' do
-    context '必須項目が揃っているとき' do
-      it '有効である' do
+  describe "バリデーション" do
+    context "必須項目が揃っているとき" do
+      it "有効である" do
         expect(build(:user)).to be_valid
       end
     end
 
-    context 'emailがないとき' do
-      it '無効である' do
+    context "emailがないとき" do
+      it "無効である" do
         user = build(:user, email: nil)
         expect(user).to be_invalid
         expect(user.errors[:email]).to be_present
+      end
+    end
+
+    context "nameがないとき" do
+      it "無効である" do
+        user = build(:user, name: "")
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to be_present
       end
     end
   end
