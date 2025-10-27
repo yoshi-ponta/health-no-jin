897003945f9177fe071664bb1a04bc43cbd7d539
yoshimaru <yoshi0410777@gmail>
2025-10-27 21:53:59 +0900

    feat:from_omniauthでのname自動設定を廃止

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_one_attached :avatar
   validate :avatar_validation
-
-  validates :name, presence: true
 
   has_many :owned_groups, class_name: "Group", foreign_key: "owner_id", dependent: :nullify
   has_many :group_memberships, dependent: :destroy
@@ -13,14 +12,17 @@ class User < ApplicationRecord
   has_many :notifications, dependent: :destroy, inverse_of: :user
   has_many :notifications_as_actor, class_name: "Notification", foreign_key: :actor_id, dependent: :nullify, inverse_of: :actor
 
+  def self.from_omniauth(auth)
+    user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
+    user.email    ||= auth.info.email
+    user.password ||= Devise.friendly_token[0, 20]
+    user
+  end
+
   private
   def avatar_validation
     return unless avatar.attached?
-    unless avatar.content_type&.start_with?("image/")
-      errors.add(:avatar, "は画像ファイルを選んでください")
-    end
-    if avatar.byte_size > 5.megabytes
-      errors.add(:avatar, "は5MB以下にしてください")
-    end
+    errors.add(:avatar, "は画像ファイルを選んでください") unless avatar.content_type&.start_with?("image/")
+    errors.add(:avatar, "は5MB以下にしてください") if avatar.byte_size > 5.megabytes
   end
 end
