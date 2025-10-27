2d061381e9767efd14c449c225c289db1cd00bba
yoshimaru <yoshi0410777@gmail>
2025-10-27 21:47:51 +0900

    feat:DeviseのOmniAuth設定とルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
     unauthenticated do
       root to: "top#index", as: :unauthenticated_root
