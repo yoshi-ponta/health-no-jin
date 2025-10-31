a6f45860a0589f4856e3312357bd47f9f6c29149
yoshimaru <yoshi0410777@gmail>
2025-10-31 17:26:34 +0900

    feat: default_meta_tags を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,30 @@ module ApplicationHelper
       concat content_tag(:span, label, class: "leading-none")
     end
   end
+
+  def default_meta_tags
+    image_for_ogp = image_url("ogp/default_ogp.png")
+    base = {
+      site: 'Health-no-Jin',
+      title: 'ヘルスノ陣',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ヘルスノ陣は、家族や友人とグループを作り、毎日の運動をポイント化！集めたポイントで「城」を育てながら、楽しく健康寿命を延ばす為のサービスです。',
+      canonical: request.original_url,
+      separator: '｜',
+      og: {
+        site_name: 'ヘルスノ陣',
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_for_ogp,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary',
+        image: image_for_ogp
+      }
+    }
+  end
 end
