44db292ba577d69ec7ff8960ce46fcae6f0ec9ec
yoshimaru <yoshi0410777@gmail>
2025-11-02 11:57:34 +0900

    feat:編集の保存時に、フラッシュメッセージを追加

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -10,4 +10,19 @@ class MypagesController < ApplicationController
     @user = current_user
     @memberships = current_user.group_memberships.active.includes(:group)
   end
+
+  def update
+    @user = current_user
+    if @user.update(profile_params)
+      redirect_to mypage_path, notice: "プロフィールを更新しました。"
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
 end
