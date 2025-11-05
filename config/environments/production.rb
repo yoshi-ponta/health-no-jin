8bc3f2f0cdac8bb4f677e9c061b297f9f0e9d3ef
yoshimaru <yoshi0410777@gmail>
2025-11-05 10:55:58 +0900

    fix:ImageMagickを導入しActiveStorageをMiniMagickに設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
+  config.active_storage.variant_processor = :mini_magick
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
