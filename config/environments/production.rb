375d163c4e83d71f2144e15e8001519b155a23f5
よしまる <yoshi0410777@gmail.com>
2025-11-07 15:38:08 +0900

    update:招待リンクを絶対URL化し、外部アプリで開けるよう修正 (#146)
    Co-authored-by: yoshimaru <yoshi0410777@gmail>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "health-no-jin.onrender.com", protocol: "https" }
+  Rails.application.routes.default_url_options = { host: "health-no-jin.onrender.com", protocol: "https" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   config.action_mailer.raise_delivery_errors = true
